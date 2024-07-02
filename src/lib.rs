8dcaf15ae4c8db449869b80e4f8cb2600b28efb7
cyphersnake <cyphersnake@pm.me>
2024-07-02 16:00:24 +0200

    add selector

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use std::marker::PhantomData;
 use halo2_proofs::{
     circuit::{Layouter, SimpleFloorPlanner, Value},
     pasta::group::ff::PrimeField,
-    plonk::{Advice, Circuit, Column, ConstraintSystem, TableColumn},
+    plonk::{Advice, Circuit, Column, ConstraintSystem, Expression, Selector, TableColumn},
     poly::Rotation,
 };
 
@@ -25,10 +25,12 @@ impl<const L: usize, F: PrimeField> BracketCircuit<L, F> {
 // Stores the configuration of the table (columns) that the circuit needs
 #[derive(Clone)]
 pub struct Config {
+    s_input: Selector,
     // For input
     input: Column<Advice>,
     // For allowed ASCII codes
     allowed: TableColumn,
+    accum: Column<Advice>,
 }
 
 impl<const L: usize, F: PrimeField> Circuit<F> for BracketCircuit<L, F> {
@@ -43,9 +45,24 @@ impl<const L: usize, F: PrimeField> Circuit<F> for BracketCircuit<L, F> {
 
     fn configure(meta: &mut ConstraintSystem<F>) -> Self::Config {
         let config = Config {
+            s_input: meta.selector(),
             input: meta.advice_column(),
             allowed: meta.lookup_table_column(),
+            accum: meta.advice_column(),
         };
+
+        // f(x) = 81 - 2*input
+        meta.create_gate("", |meta| {
+            let _81 = Expression::Constant(F::from(81));
+            let _2 = Expression::Constant(F::from(2));
+
+            let s_input = meta.query_selector(config.s_input);
+            let input = meta.query_advice(config.input, Rotation::cur());
+            let prev = meta.query_advice(config.accum, Rotation::cur());
+            let result = meta.query_advice(config.accum, Rotation::next());
+
+            vec![s_input * (prev + (_81 - _2 * input) - result)]
+        });
 
         meta.lookup(|table| {
             let input = table.query_advice(config.input, Rotation::cur());
